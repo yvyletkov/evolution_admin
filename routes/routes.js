b6b485bdb9bea51f38391eda496a58080c09a2c6
Yaroslav Vyletkov <yvyletkov@ya.ru>
2021-02-19 12:44:37 +0300

    ad redirect

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,10 @@ const eventsRoutes = require('./events-routes')
 router.use(newsRoutes);
 router.use(eventsRoutes);
 
+router.get('/', (req, res) => {
+    res.redirect('/evo/news')
+})
+
 module.exports.router = router;
 
 
